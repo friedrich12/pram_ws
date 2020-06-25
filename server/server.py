1941616bc0f2e9716a37de0dffbc953d03032c4b
professor <friedrichdoku@gmail.com>
2020-06-25 08:48:37 -0500

    Directory changes

---
+++ server/server.py
@@ -0,0 +1,17 @@
+import random
+import string
+import cherrypy
+
+
+class PramWebServer(object):
+    @cherrypy.expose
+    def hello(self):
+        return "Hello World!"
+
+    @cherrypy.expose
+    def simple(self):
+        return "Running simple test"
+
+
+if __name__ == '__main__':
+    cherrypy.quickstart(StringGenerator())
